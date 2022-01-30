e47ee5e9577103a70f41393354031388c9f99dc3
mrahu27 <mrahul27@gmail.com>
2022-01-30 13:59:24 +0530

    commit for command line command_line_args

---
+++ command_line_args.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+my_script_name=$0
+echo "My script name is : ${my_script_name}"
+#here above $0 is special variable in shell scripting which will always denote present running shell script file name.
